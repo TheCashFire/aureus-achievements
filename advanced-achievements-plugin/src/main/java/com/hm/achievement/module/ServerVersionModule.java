ecc4ba0a91d89c7d98d763c7aaf840939014dfbf
petulikan1 <petulikan@gmail.com>
2024-07-16 19:49:24 +0200

    Fixed method to obtain server version the correct way
    Signed-off-by: petulikan1 <petulikan@gmail.com>
    
    Took 2 minutes

--- advanced-achievements-plugin/src/main/java/com/hm/achievement/module/ServerVersionModule.java
+++ advanced-achievements-plugin/src/main/java/com/hm/achievement/module/ServerVersionModule.java
@@ -2,7 +2,6 @@ package com.hm.achievement.module;
 
 import javax.inject.Singleton;
 
-import org.apache.commons.lang3.StringUtils;
 import org.bukkit.Bukkit;
 
 import dagger.Module;
@@ -18,10 +17,10 @@ public class ServerVersionModule {
 		// versions change in the future.
 
 		// petulikan1
-		// Bukkit.getBukkitVersion() -> 1.20.4-R0.1-SNAPSHOT
-		// split("-")[0] -> 1.20.4
-		// substringBetween(arg,".") -> 20
-		return Integer.parseInt(StringUtils.substringBetween(Bukkit.getBukkitVersion().split("-")[0], "."));
+		// Bukkit.getBukkitVersion() -> 1.20.4-R0.1-SNAPSHOT || 1.21-R0.1-SNAPSHOT
+		// split("-")[0] -> 1.20.4 | 1.21
+		// split("\\.") -> 20 | 21
+		return Integer.parseInt(Bukkit.getVersion().split("-")[0].split("\\.")[1]);
 	}
 
 }
